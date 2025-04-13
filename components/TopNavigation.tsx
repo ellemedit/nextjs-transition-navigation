4770241ecf1f5c4bbc2ccd7ae7edf2c442b9afff
Marshall Han <beingbook@gmail.com>
2025-04-13 14:18:32 +0900

    wip

--- components/TopNavigation.tsx
+++ components/TopNavigation.tsx
@@ -6,18 +6,22 @@ import { twMerge } from "tailwind-merge";
 
 import "./TopNavigation.css";
 
-export function TopNavigation(props: ComponentPropsWithRef<"div">) {
+export function TopNavigation({
+  className,
+  children,
+  ...otherProps
+}: ComponentPropsWithRef<"div">) {
   return (
     <ViewTransition name="top-navigation">
       <div
-        {...props}
+        {...otherProps}
         className={twMerge(
           "sticky top-0 px-8 sm:px-[10vw] py-4 bg-white border-b-2 border-black z-10",
-          props.className
+          className
         )}
       >
         <ViewTransition name="top-navigation-content">
-          <div>{props.children}</div>
+          <div className={twMerge(className)}>{children}</div>
         </ViewTransition>
       </div>
     </ViewTransition>
